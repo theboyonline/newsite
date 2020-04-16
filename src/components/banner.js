678eb6c14092110a53d4cbda036aeb284d20fd07
theboyonline <32967531+theboyonline@users.noreply.github.com>
2020-04-16 16:50:33 +0530

    header change

---
+++ src/components/banner.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { useStaticQuery, graghpl } from "gatsby"
+import Img from "gatsby-image"
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="container">
+        <div className="row">
+          <h1 className="maintext">Damn That was</h1>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <h1 className="mainbelow">FAST...</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 //scss
 import "../styles/styles.scss"
 import Header from "../components/header"
+import Banner from "../components/banner"
 
 const IndexPage = () => (
   <div>
     <Header />
+    <Banner />
   </div>
 )
 
