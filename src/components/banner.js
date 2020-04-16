3bf630a019169f84707821f58402f2d54cfe40bb
theboyonline <32967531+theboyonline@users.noreply.github.com>
2020-04-16 17:24:16 +0530

    image

--- src/components/banner.js
+++ src/components/banner.js
@@ -7,14 +7,7 @@ const Banner = () => {
     <div className="banner">
       <div className="container">
         <div className="row">
-          <h1 className="maintext">Damn That was</h1>
-        </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <div className="row">
-          <h1 className="mainbelow">FAST...</h1>
+          <h1 className="maintext">Damn That was fast..</h1>
         </div>
       </div>
     </div>
